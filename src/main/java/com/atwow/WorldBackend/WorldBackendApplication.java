0715d6f598e7c8742a7bc9f1d49b3ef799e14094
kvipka90 <kvipka90@yandex.ru>
2023-01-05 19:34:52 +0300

    init springboot 3.0.1

---
+++ src/main/java/com/atwow/WorldBackend/WorldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atwow.WorldBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atwow/WorldBackend/WorldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwow.WorldBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
